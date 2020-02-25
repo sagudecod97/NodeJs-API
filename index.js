ac42210253b23e1175a46a96f85a2f8d26e60529
Santiago <sagudecod97@gmail.com>
2020-02-24 22:48:52 -0500

    Parsed the incoming url request and trimmed the slashes at beginning and at the end

--- index.js
+++ index.js
@@ -2,11 +2,21 @@
  * Main file for the API
  */
 
- const http = require('http')
- const server = http.createServer((req, res) => {
-     res.end('Hello World\n')
- })
+const http = require('http')
+const url = require('url')
 
- server.listen(3000, () => {
-     console.log('Server listening on port 3000')
- })
+const server = http.createServer((req, res) => {
+    // First Parse the URL
+    let parsedURL = url.parse(req.url, true)
+    
+    // Getting the URL
+    let path = parsedURL.pathname
+    let trimmedPath = path.replace(/^\/+|\/+$/g, '')
+    console.log(trimmedPath)
+
+    res.end('Hello World\n')
+})
+
+server.listen(3000, () => {
+    console.log('Server listening on port 3000')
+})
